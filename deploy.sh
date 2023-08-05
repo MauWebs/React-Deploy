bda53f5da59f12ec89dae96f5b9432535ec3b4c9
MauWebs <mauriciovidalwebs@gmail.com>
2023-08-05 08:57:10 -0300

    commit

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env/ sh
+
+set -e
+
+cd dist
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+git remote add origin https://github.com/MauWebs/React-Deploy.git
+
+git push -f origin HEAD:gh-pages
